d381c8abcff25d5682458e75a4ad8e650a0bba93
Andre Mesquita <avmesquita@gmail.com>
2023-03-21 18:43:10 +0000

    compression

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const serve   = require('express-static');
 const favicon = require('serve-favicon');
+const compression = require('compression');
 
 const app = express();
- 
+
+app.use(compression());
 app.use(favicon(__dirname + '/wwwroot/favicon.ico'));
 app.use(serve(__dirname + '/wwwroot/index.html'));
 
